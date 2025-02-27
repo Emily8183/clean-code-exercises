f6aa3f194076005cfc16a4448452b335b7f08143
Emily <emily.zy.lin@gmail.com>
2025-02-27 14:09:55 -0600

    complete TextHelper

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -3,10 +3,30 @@ package com.b.simple.design.business.text;
 public class TextHelper {
 
 	public String swapLastTwoCharacters(String str) {
-		return null;
+
+		int length = str.length();
+
+		if (length < 2) return str;
+
+		char lastChar = str.charAt(length-1);
+		char secondLastChar = str.charAt(length-2);
+
+		String restOfTheString = str.substring(0, length-2);
+
+		return restOfTheString + lastChar + secondLastChar;
+		
 	}
 
 	public String truncateAInFirst2Positions(String str) {
-		return null;
+		if (str.length() < 2) return str.replaceAll("A","");
+
+		String firstTwoCharacters = str.substring(0,2);
+
+		String restOfString = str.substring(2);
+
+		String updatedFirstTwoCharacters = firstTwoCharacters.replaceAll("A", "");
+		//String is immutable, so have to assign to a new variable
+
+		return updatedFirstTwoCharacters + restOfString;
 	}
 }
